e0f27723a9dc37a1b52388614b6706de037e198f
Yash <yashkantharia13@gmail.com>
2023-08-01 22:32:49 +0530

    Initial footer col

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,28 +3,28 @@ import React from 'react';
 function Footer ()  {
   
   return (
-    <div>
+    <div className='bg-dark !important'>
         <footer className=" text-center text-lg-start ">
   {/* <!-- Grid container --> */}
-  <div className="container p-4 bg-dark">
+  <div className="container p-4 footerdiv">
     {/* <!--Grid row--> */}
     <div className="row ">
       {/* <!--Grid column--> */}
-      <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 className="text-uppercase">Icons</h5>
+      <div className="col-lg-3 col-md-6 mb-4 mb-md-0 ">
+        <h5 className="text-uppercase text-secondary">Icons</h5>
 
-        <ul className="list-unstyled mb-0">
+        <ul className="list-unstyled mb-0 ">
           <li>
-            <a href="#!" className="text-dark">Link 1</a>
+            <a href="text-secondary"  >Link 1</a>
           </li>
           <li>
-            <a href="#!" className="text-dark">Link 2</a>
+            <a href="text-secondary" >Link 2</a>
           </li>
           <li>
-            <a href="#!" className="text-dark">Link 3</a>
+            <a href="text-secondary" >Link 3</a>
           </li>
           <li>
-            <a href="#!" className="text-dark">Link 4</a>
+            <a href="text-secondary"  >Link 4</a>
           </li>
         </ul>
       </div>
@@ -32,20 +32,20 @@ function Footer ()  {
 
       {/* <!--Grid column--> */}
       <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 className="text-uppercase mb-0">Kicks</h5>
+        <h5 className="text-uppercase mb-0 text-secondary">Kicks</h5>
 
         <ul className="list-unstyled">
           <li>
-            <a href="#!" className="text-secondary">Air Force 1</a>
+            <a href="text-secondary" >Air Force 1</a>
           </li>
           <li>
-            <a href="#!" className="text-secondary">Air Max 90</a>
+            <a href="text-secondary" >Air Max 90</a>
           </li>
           <li>
-            <a href="#!" className="text-secondary">Air  Max 95</a>
+            <a href="text-secondary" >Air  Max 95</a>
           </li>
           <li>
-            <a href="#!" className="text-secondary">Huarache</a>
+            <a href="text-secondary" >Huarache</a>
           </li>
         </ul>
       </div>
@@ -53,20 +53,20 @@ function Footer ()  {
 
       {/* <!--Grid column--> */}
       <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 className="text-uppercase">Clothing</h5>
+        <h5 className="text-uppercase text-secondary">Clothing</h5>
 
         <ul className="list-unstyled mb-0">
           <li>
-            <a href="#!" className="text-dark">Link 1</a>
+            <a href="text-secondary" >Link 1</a>
           </li>
           <li>
-            <a href="#!" className="text-dark">Link 2</a>
+            <a href="text-secondary" >Link 2</a>
           </li>
           <li>
-            <a href="#!" className="text-dark">Link 3</a>
+            <a href="text-secondary" >Link 3</a>
           </li>
           <li>
-            <a href="#!" className="text-dark">Link 4</a>
+            <a href="text-secondary" >Link 4</a>
           </li>
         </ul>
       </div>
@@ -74,20 +74,20 @@ function Footer ()  {
 
       {/* <!--Grid column--> */}
       <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 className="text-uppercase mb-0">Kids</h5>
+        <h5 className="text-uppercase mb-0 text-secondary">Kids</h5>
 
         <ul className="list-unstyled">
           <li>
-            <a href="#!" className="text-dark">Link 1</a>
+            <a href="text-secondary">Link 1</a>
           </li>
           <li>
-            <a href="#!" className="text-dark">Link 2</a>
+            <a href="text-secondary">Link 2</a>
           </li>
           <li>
-            <a href="#!" className="text-dark">Link 3</a>
+            <a href="text-secondary">Link 3</a>
           </li>
           <li>
-            <a href="#!" className="text-dark">Link 4</a>
+            <a href="text-secondary">Link 4</a>
           </li>
         </ul>
       </div>
